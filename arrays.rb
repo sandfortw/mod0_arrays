2e70a67b0f16797c8685f42119d812c61a1219ac
Weston Sandfort <sandfortw@gmail.com>
2022-11-02 14:57:13 -0600

    Explain index positions.

--- arrays.rb
+++ arrays.rb
@@ -18,3 +18,7 @@ print floats
 #Will add "false" to left side of the array and print the new array.
 bools.unshift "false"
 print bools
+
+#Index positions are the positions of each element in the array, starting with
+#0 and counting upwards. For example, in the ints array I created, the element 1
+#was in position 0.
