3f7c926a07ef61d1510769ea9fbdcebce1b80a32
Weston Sandfort <sandfortw@gmail.com>
2022-11-02 14:53:50 -0600

    Edit each of the arrays created

--- arrays.rb
+++ arrays.rb
@@ -3,3 +3,18 @@ ints = [1, 2, 3, 4]
 floats = [1.1, 2.2, 3.3, 4.4]
 bools = [true, false, true, false]
 
+#Will add "five" to the end of strings array and print the new array.
+strings.push "five"
+print strings
+
+#Will remove "4" from the end of ints array and print the new array.
+ints.pop
+print ints
+
+#Will remove "1.1" from floats array and print the new array.
+floats.shift
+print floats
+
+#Will add "false" to left side of the array and print the new array.
+bools.unshift "false"
+print bools
